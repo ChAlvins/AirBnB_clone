1137a070679379af587af52d21d849948cc65146
vpnchengo <charo.alvin@gmail.com>
2023-03-11 16:21:12 +0000

    init_update

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""__init__ magic method for models directory"""
-from models.engine.filestorage import FileStorage
-=======
 """package for models directory"""
 from models.engine.file_storage import FileStorage
->>>>>>> 350e0caa10a0d522be9904841b0149ab52278d01
 
 
 storage = FileStorage()
