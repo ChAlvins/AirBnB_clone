40d776f167e3c581cd2b7ec001e85fd18ad77d79
samuelsenerwa <samuelsenerwa@gmail.com>
2023-03-13 12:11:57 -0700

    fixed console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -42,7 +42,7 @@ class FileStorage:
             with open(FileStorage.__file_path) as f:
                 objdict = json.load(f)
                 for o in object.values():
-                    cls_name = o["__class__"}
+                    cls_name = o["__class__"]
                     del o["__class__"]
                     self.new(eval(cls_name)(**o))
         except FileNotFoundError:
