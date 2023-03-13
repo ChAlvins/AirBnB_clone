ca216e069b09651e74235e26e552a3490e9fd954
samuelsenerwa <samuelsenerwa@gmail.com>
2023-03-13 10:13:35 -0700

    corrected errors

--- console.py
+++ console.py
@@ -14,7 +14,7 @@ from models.city import City
 
 
 def parse(self):
-    curly_braces = re.search(r"\[(.*?)\]", arg)
+    curly_braces = re.search(r"\{(.*?)\}", arg)
     brackets = re.search(r"\[(.*?)\]", arg)
     if curly_braces is None:
         if brackets is None:
@@ -22,7 +22,7 @@ def parse(self):
         else:
             lexer = split(arg[:brackets.span()[0]])
             retl = [i.strip(",") for i in lexer]
-            ret1.append(brackets.group())
+            retl.append(brackets.group())
             return retl
     else:
         lexer = split(args[:curly_braces.span()[0]])
@@ -54,7 +54,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def default(self, arg):
-        """Defaultbehavior for cmd module when input is valid """
+        """Default behavior for cmd module when input is valid """
         argdict = {
                 "all": self.do_all,
                 "show": self.do_show,
@@ -90,10 +90,10 @@ class HBNBCommand(cmd.Cmd):
         argl = parse(arg)
         if len(argl) == 0:
             print("** class name missing **")
-        elif argl[0] not in HBNBCommand__classes:
+        elif argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         else:
-            print(eval(argsl[0]().id))
+            print(eval(argl[0]().id))
             storage.save()
 
     def do_show(self, arg):
@@ -141,7 +141,7 @@ class HBNBCommand(cmd.Cmd):
             obj1 = []
             for obj in storage.all().values():
                 if len(argl) > 0 and argl[0] == obj.__class__.__name__:
-                    objl.append(objl.__str__())
+                    objl.append(obj.__str__())
                 elif len(argl) == 0:
                     objl.append(obj.__str__())
             print(objl)
@@ -174,7 +174,7 @@ class HBNBCommand(cmd.Cmd):
         if len(argl) == 1:
             print("** instance id is missing **")
             return False
-        if "{}.{}".format(arg[0], argl[1]) not in objdict.keys():
+        if "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
             print("** no instance found **")
             return False
         if len(argl) == 2:
@@ -191,13 +191,13 @@ class HBNBCommand(cmd.Cmd):
             obj = objdict["{}.{}".format(argl[0], argl[1])]
             if argl[2] in obj.__class__.__dict__.keys():
                 valtype = type(obj.__class__.__dict__[argl[2]])
-                obj.__dict__[argsl[2]] = valtype(argsl[3])
+                obj.__dict__[argl[2]] = valtype(argl[3])
             else:
                 obj.__dict__[argl[2]] = argl[3]
-        elif type(eval(argsl[2])) == dict:
+        elif type(eval(argl[2])) == dict:
             obj = objdict["{}.{}".format(argl[0], argl[1])]
             for k, v in eval(argl[2]).items():
-                if (k in obj.__class__.__dict__keys() and
+                if (k in obj.__class__.__dict__.keys() and
                         type(obj.__class__.__dict__[k]) in {str, int, float}):
                     valtype = type(obj.__class__.__dict__[k])
                     obj.__dict__[k] = valtype(v)
